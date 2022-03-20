0322598d945a76df5398a5f2d987bd2608e26a96
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2022-03-20 21:14:31 +0530

    JS comment

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const panels = document.querySelectorAll('.panel')
 
+// Here what we are basically trying to do is when the "click" event happens we remove "active" classes from all the divs and then "active" class to the one which was clicked
 panels.forEach(panel => {
     panel.addEventListener("click", function () {
         removeActiveClasses()
